812b5b08050bd2fbd79d470185db6a14fb0102e3
yeawonbong <ybong.develop@gmail.com>
2022-06-12 15:14:39 +0900

    ~Part1

--- blog/src/App.js
+++ blog/src/App.js
@@ -4,22 +4,10 @@ import './App.css';
 function App() {
 
   let [글제목, 글제목변경] = useState(['강남 맛집 추천', '광화문 맛집 추천', '합정 맛집 추천']);
+  let [발행일, 발행일변경] = useState(['2월 17일', '2월 17일', '2월 17일'])
   let [따봉, 따봉변경] = useState([0, 0, 0]);
   let [modal, setModal] = useState(-1);
   let [input, setInput] = useState('');
-
-//   function post(no) {
-// 	return (
-// 	  <div className="list">
-// 		<h3 onClick={()=>{setModal(!modal)}}> 
-// 			{ 글제목[no] } 
-// 		</h3>  
-// 		<span onClick={ ()=>{ 따봉더하기(no) } }>👍</span> { 따봉[no] } 
-// 		<p>2월 17일 발행</p>
-// 		<hr/>
-// 	  </div>
-// 	)
-//   }
 
   function 따봉더하기(no) {
 	var newArray = [...따봉];
@@ -39,10 +27,6 @@ function App() {
 		<div>개발 Blog</div>
 	  </div>
 	  <br/>
-	  {/* <button onClick={()=>{글제목바꾸기(0, '뉴욕 맛집 추천')}}>글 수정</button> */}
-	  {/* {post(0)}
-	  {post(1)}
-	  {post(2)} */}
 	{
 		글제목.map(function(title, i) {
 			return (
@@ -51,7 +35,7 @@ function App() {
 					  { title } 
 				  </h3>  
 				  <span onClick={ ()=>{ 따봉더하기(i) } }>👍</span> { 따봉[i] } 
-				  <p>2월 17일 발행</p>
+				  <p>{발행일[i]} 발행</p>
 				  <button onClick={()=>{
 					  let temparr = [...글제목];
 					  temparr.splice(i,1);
@@ -60,14 +44,24 @@ function App() {
 				  <hr/>
 				</div>
 			  )
-			})
+			}
+			)
 		}
 
 		<input type="text" onChange={(e)=>{
 			setInput(e.target.value); 
 			console.log(input)
 		}}/>
-		<button onClick={()=>{글제목변경([...글제목, input]); 따봉변경([...따봉, 0])}}>발행</button>
+		<button onClick={()=>{
+			if (input !== '') {
+				글제목변경([input, ...글제목]); 
+				발행일변경([`${new Date().getMonth()+1}월 ${new Date().getDate()}일`, ...발행일])
+				따봉변경([0, ...따봉])
+			}
+			else
+			alert('내용을 입력해주세요.')
+		}}>발행</button>
+		<Profile/>
 		{//JS로 조건식을 쓴다
 		  modal >= 0 ? <Modal 글제목={글제목} no={modal} 글제목바꾸기={글제목바꾸기}/> : null
 		}
@@ -75,7 +69,6 @@ function App() {
 
   );
 }
-
 
 function Modal(props) {
   return (
@@ -86,6 +79,26 @@ function Modal(props) {
 	<button onClick={()=>{props.글제목바꾸기(props.no, '뉴욕 맛집 추천')}}>글수정</button>
   </div>
   )
+}
+
+class Profile extends React.Component { //옛날 React
+	constructor() {
+		super();
+		this.state = { name: 'Kim', age : 30 } //state 선언은 이렇게 
+	}
+
+	changeName(){
+		this.setState({name: 'Park'})
+	}
+	render() {
+		return (
+			<div>
+				<h3>프로필입니다</h3>
+				<p>저는 { this.state.name } 입니다.</p>
+				<button onClick={ this.changeName.bind(this) }>버튼</button>
+			</div>
+		)
+	}
 }
 
 export default App;
